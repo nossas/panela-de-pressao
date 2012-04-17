fbc1759a4be82e3efafc53aa282025c02a3e361c
Diogo Biazus <diogob@gmail.com>
2012-04-17 17:45:16 -0300

    added sessions controller form vouc code with specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user, :signed_in?
 
   protected
   def render_404
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.id
   end
 end
 
