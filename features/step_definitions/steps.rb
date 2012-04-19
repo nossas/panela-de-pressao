fef324184ef60e14a73f57fd13fdd57ce414c6d4
Nícolas Iensen <nicolas@quavio.com.br>
2012-04-19 17:54:58 -0300

    #4 Add inherited resources. Add campaign partial

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,6 +2,14 @@ Given /^I'm in the campaigns page$/ do
   visit campaigns_path
 end
 
+Given /^there is a campaign called "([^"]*)"$/ do |arg1|
+  Campaign.make! name: arg1
+end
+
 Then /^I should see "([^"]*)"$/ do |arg1|
   page.should have_content(arg1)
+end
+
+When /^I go to the campaigns page$/ do
+  visit campaigns_path
 end
