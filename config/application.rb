dabaf80f2e5704e32516be6152ef3b943d87a258
Luiz Fonseca <runeroniek@gmail.com>
2012-04-20 11:20:45 -0300

    Prepare the assets pipeline and make it work.

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(:default, :assets, Rails.env) if defined?(Bundler)
 
 module ManifesteSe
   class Application < Rails::Application
@@ -35,6 +35,14 @@ module ManifesteSe
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+
+    
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
