bc2d24d90b51b7590a04b8baf0c661eeb75c9a90
Nícolas Iensen <nicolas@quavio.com.br>
2012-04-24 20:14:14 -0300

    #1 change the scenario where the user is not logged in to simulate the whole scenario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :signed_in?
 
+  before_filter {|controller| session[:restore_url] = request.url if controller.controller_name != "sessions" }
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_session_path, :alert => exception.message
   end
