4a8a5de22b3ce9d25bf2c7162f3e8f351580e6c0
Luiz Fonseca <runeroniek@gmail.com>
2012-04-25 10:43:35 -0400

    Fixes. Also, use file for development storage. No need of disable processing as we are not using any lib to deal with crop and stuff yet.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,17 +11,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.test? or Rails.env.cucumber?
-    storage :file
-    enable_processing = false
-  else
-    storage :fog
-  end
+  storage :file unless Rails.env.production?
+  storage :fog if Rails.env.production?
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -42,12 +44,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process :scale => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
   # end
 
   # Override the filename of the uploaded files:
