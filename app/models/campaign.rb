10eccddae69e6bc9e3e53e6d94d42232cece707d
Diogo Biazus <diogob@gmail.com>
2012-04-26 14:10:37 -0300

    added some specs to cover validations in campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -11,5 +11,5 @@ class Campaign < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
-  validates :name, :description, :user_id, :image, :presence => true
+  validates :name, :description, :user_id, :image, :category_id, :presence => true
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -6,4 +6,12 @@ describe Campaign do
     it{ should belong_to :organization }
     it{ should belong_to :category }
   end
+
+  describe "validations" do
+    it{ should validate_presence_of :name }
+    it{ should validate_presence_of :description }
+    it{ should validate_presence_of :user_id }
+    it{ should validate_presence_of :image }
+    it{ should validate_presence_of :category_id }
+  end
 end
