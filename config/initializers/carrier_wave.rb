37d6324ddb5c05d9d7667780cbfa0a5c0962c44b
Luiz Fonseca <runeroniek@gmail.com>
2012-04-26 18:06:31 -0400

    Fix initializer for carrierwave and storage setting on the image uploader.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,9 @@
-if Rails.env.test? || Rails.env.cucumber?
-  CarrierWave.configure do |config|
-    config.storage = :file
-  end
-else
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV["AWS_ID"],
-      :aws_secret_access_key  => ENV["AWS_SECRET"]
-    }
-    config.fog_directory  = 'manifeste-se'
-  end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV["AWS_ID"],
+    :aws_secret_access_key  => ENV["AWS_SECRET"],
+  }
+  config.fog_directory  = 'manifeste-se' 
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
