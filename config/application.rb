d4d0f57d3f226861027c9acca4cc5152137bf32d
Luiz Fonseca <runeroniek@gmail.com>
2012-04-26 18:56:28 -0400

    Don't initialize the app while precompiling, dude.

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,7 @@ module ManifesteSe
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
