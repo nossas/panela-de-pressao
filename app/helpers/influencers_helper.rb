50273f72e65fa9a18ffaabccbbdcf01c9f79bbb0
Nícolas Iensen <nicolas@quavio.com.br>
2012-04-29 12:38:03 -0300

    #14 Add an influencers index page and a create form

---
+++ app/helpers/influencers_helper.rb
@@ -0,0 +1,2 @@
+module InfluencersHelper
+end
--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -2,6 +2,8 @@ class Influencer < ActiveRecord::Base
   attr_accessible :email, :facebook, :name, :twitter, :role
   has_many :targets
   has_many :campaigns, :through => :targets
+  default_scope order("name")
+  validates :name, :role, :presence => true
 
   def to_s
     "#{self.name} (#{self.role})"
