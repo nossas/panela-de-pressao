056f1c934fde5056cf357eb8a4122362131e0dd6
Nícolas Iensen <nicolas@quavio.com.br>
2012-05-01 11:29:13 -0300

    Fix test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,6 +6,7 @@ describe SessionsController do
   describe "GET /auth/facebook/callback" do
     before do
       OmniAuth.config.add_mock(:facebook, {:uid => '12345'})
+      Authorization.stub(:create_from_hash).and_return(stub_model(Authorization, :user => stub_model(User, :id => 12345)))
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook] 
       get :create, :provider => 'facebook'
     end
