3ba2c28552092ee1b2cc0a2864950f9290be4126
Nícolas Iensen <nicolas@quavio.com.br>
2012-05-02 22:28:45 -0300

    #11 Bug fix for encoding

--- app/mailers/poke_mailer.rb
+++ app/mailers/poke_mailer.rb
@@ -4,7 +4,7 @@ class PokeMailer < ActionMailer::Base
   def poke(the_poke)
     @poke = the_poke
     mail(
-      :to => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>"}, 
+      :to => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>"}.join(", "), 
       :subject => the_poke.campaign.name,
       :from => "\"#{the_poke.user.name}\" <#{the_poke.user.email}>"
     )
