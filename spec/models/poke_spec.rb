c6afb01e646550fbc03374063e31264111ef30d0
Diogo Biazus <diogob@gmail.com>
2012-05-06 15:19:55 -0300

    added twitter? method to poke

--- spec/models/poke_spec.rb
+++ spec/models/poke_spec.rb
@@ -1,6 +1,30 @@
 require 'spec_helper'
 
 describe Poke do
+  describe "#twitter?" do
+    context "when kind is other than twitter" do
+      subject{ Poke.new(:kind => 'email').twitter? }
+      it{ should be_false }
+    end
+
+    context "when kind is twitter" do
+      subject{ Poke.new(:kind => 'twitter').twitter? }
+      it{ should be_true }
+    end
+  end
+
+  describe "#email?" do
+    context "when kind is other than email" do
+      subject{ Poke.new(:kind => 'twitter').email? }
+      it{ should be_false }
+    end
+
+    context "when kind is email" do
+      subject{ Poke.new(:kind => 'email').email? }
+      it{ should be_true }
+    end
+  end
+
   describe "#update_influencers_poke_counter" do
     let(:target1){ stub_model(Target) }
     let(:target2){ stub_model(Target) }
