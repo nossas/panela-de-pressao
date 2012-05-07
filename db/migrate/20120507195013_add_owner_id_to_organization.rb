b33edea51d3aad6cf9de8aa8a7e092208274c072
Luiz Fonseca <runeroniek@gmail.com>
2012-05-07 16:59:36 -0300

    Fix migration.

--- db/migrate/20120507195013_add_owner_id_to_organization.rb
+++ db/migrate/20120507195013_add_owner_id_to_organization.rb
@@ -1,7 +1,7 @@
 class AddOwnerIdToOrganization < ActiveRecord::Migration
   def change
     add_column :organizations, :owner_id, :integer, null: false
+    add_foreign_key :organizations, :users, column: 'owner_id'
   end
 
-  add_foreign_key :organizations, :users, column: 'owner_id'
 end
