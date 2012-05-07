7a63a161127dc6cff878d6aa700fdbd6419fab56
Diogo Biazus <diogob@gmail.com>
2012-05-07 16:25:36 -0300

    moved poke creation from sesions controller to poke controller

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -1,14 +1,23 @@
 # coding: utf-8
 class PokesController < InheritedResources::Base
-  load_and_authorize_resource
+  load_and_authorize_resource :only => [:create]
   belongs_to :campaign
 
   prepend_before_filter(:only => [:create]) do 
     session[:poke] = params[:poke].merge(:campaign_id => params[:campaign_id]) if current_user.nil? && params[:poke]
   end
 
+  before_filter do
+    params[:poke] ||= {}
+    params[:poke][:user_id] = current_user.id
+  end
+  
+  def create_from_session
+    params[:poke] = session.delete(:poke)
+    create
+  end
+
   def create
-    resource.user = current_user
     create! do |success, failure|
       success.html { redirect_to @campaign, :notice => "Seu email foi enviado aos alvos da campanha, é isso aí! Pressão neles!" }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ ManifesteSe::Application.routes.draw do
   match '/auth/:provider/callback',   :to => 'sessions#create'
   resources :sessions, :only => [:new, :destroy]
   resources :campaigns, :only => [:index, :show, :new, :create] do
-    resources :pokes, :only => [:create]
+    resources :pokes, :only => [:create] do
+      collection do
+        get :create_from_session
+      end
+    end
   end
   resources :influencers, :only => [:index, :create]
   root :to => 'campaigns#index'
