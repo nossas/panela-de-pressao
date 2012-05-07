3eacfca0e016517ad3417acecab31b5555b542fb
Diogo Biazus <diogob@gmail.com>
2012-05-07 12:51:16 -0300

    moved poke store code to pokes controller. added some controller specs

--- spec/controllers/pokes_controller_spec.rb
+++ spec/controllers/pokes_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe PokesController do
+  subject{ response }
+
+  describe "POST create" do
+    let(:poke) { { 'user_id' => User.make!.id.to_s, 'kind' => 'email' } }
+    let(:campaign) { Campaign.make! }
+
+    context "when I'm logged in and have no poke in session" do
+      before do
+        controller.stub(:current_user).and_return(User.make!)
+        post :create, :poke => poke, :campaign_id => campaign.id
+      end
+
+      it{ should redirect_to campaign_path(Campaign.last) }
+      it{ session[:poke].should == nil }
+    end
+
+    context "when I'm not logged in" do
+      before do
+        post :create, :poke => poke, :campaign_id => campaign.id
+      end
+
+      it{ should redirect_to new_session_path }
+      it{ session[:poke].should == poke.merge('campaign_id' => campaign.id.to_s) }
+    end
+  end
 
 end
