6e53de46662fc99fd2797583793c5b4ed94bd3f1
Diogo Biazus <diogob@gmail.com>
2012-05-07 12:51:16 -0300

    moved poke store code to pokes controller. added some controller specs

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -1,7 +1,11 @@
 # coding: utf-8
 class PokesController < InheritedResources::Base
   load_and_authorize_resource
-  nested_belongs_to :campaign
+  belongs_to :campaign
+
+  prepend_before_filter(:only => [:create]) do 
+    session[:poke] = params[:poke].merge(:campaign_id => params[:campaign_id]) if current_user.nil? && params[:poke]
+  end
 
   def create
     resource.user = current_user
