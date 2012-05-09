53da5f577bc281b59d823f42df4828cd504a7445
Luiz Fonseca <runeroniek@gmail.com>
2012-05-09 17:49:36 -0300

    Assure that the organization belongs to influencer.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Organization do
   describe "validations" do
-    it{ should validate_presence_of :name }
+    it { should validate_presence_of :name }
+  end
+  describe "associations" do
+    it { should belong_to :owner }
   end
 end
