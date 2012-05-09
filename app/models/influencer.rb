8cd0cfc3be638e5119fc80600b3ba8a6da413021
Luiz Fonseca <runeroniek@gmail.com>
2012-05-09 18:05:40 -0300

    Just cosmetics with associations and validations..

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -2,6 +2,7 @@ class Influencer < ActiveRecord::Base
   attr_accessible :email, :facebook, :name, :twitter, :role, :avatar, :avatar_cache
   has_many :targets
   has_many :campaigns, :through => :targets
+  has_many :organizations, :foreign_key => "owner_id"
   default_scope order("name")
   validates :name, :role, :presence => true
   mount_uploader :avatar, AvatarUploader
--- spec/models/influencer_spec.rb
+++ spec/models/influencer_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Influencer do
+  describe "associations" do
+    it { should have_many :targets }
+    it { should have_many :campaigns }
+    it { should have_many :organizations }
+  end
+  describe "validations" do 
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :role }
+  end
 end
