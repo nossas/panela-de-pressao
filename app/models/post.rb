de08a25b33bd773a4629c786922b21f4f55fc4b9
Diogo Biazus <diogob@gmail.com>
2012-05-14 17:07:30 -0300

    added auto_html in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
 
    belongs_to :campaign
    validates_presence_of :campaign_id, :content
+
+   auto_html_for :content do
+     html_escape
+     image
+     youtube(:width => 400, :height => 250)
+     link :target => "_blank", :rel => "nofollow"
+     simple_format
+   end
 end
