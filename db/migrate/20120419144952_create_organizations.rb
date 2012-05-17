e9cf42d6956f37524bf2a01bc40125d483a3f3fc
Luiz Fonseca <runeroniek@gmail.com>
2012-05-17 14:19:57 -0300

    Fix issue with missing columns on rake db:migrate task.

--- db/migrate/20120419144952_create_organizations.rb
+++ db/migrate/20120419144952_create_organizations.rb
@@ -2,7 +2,8 @@ class CreateOrganizations < ActiveRecord::Migration
   def change
     create_table :organizations do |t|
       t.text :name, :null => false
-
+      t.text :cnpj, :null => false
+      t.text :address, :null => false
       t.timestamps
     end
   end
