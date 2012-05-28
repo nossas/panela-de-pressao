3145989f24705c5d5ad4f383795b5b603b0e7448
Nícolas Iensen <nicolas@engage.is>
2012-05-28 14:50:13 -0300

    Show latest posts in campaigns#index

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,6 +6,14 @@ var App = window.App = {
       $('#campaign_twitter_text').textareaCount({maxCharacterSize: 100}, function(data){
         $(".campaign_twitter_text_status").html(100 - data.input + " caracteres");
       });
+      
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=130536417070695";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     },
 
     finish: function(){
