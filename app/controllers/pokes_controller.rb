d0361d7327e1e63a2a3cfc3a3c723048eb5b88f5
Nícolas Iensen <nicolas@engage.is>
2012-06-03 10:00:39 -0300

    Redirect with anchor after poke create

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -15,7 +15,7 @@ class PokesController < InheritedResources::Base
   def create
     @poke = Poke.new session.delete(:poke).merge(:user_id => current_user.id)
     create! do |success, failure|
-      success.html { redirect_to @campaign, :anchor => "poke_buttons" }
+      success.html { redirect_to :action => "show", :controller => "campaigns", :id => @campaign.id, :anchor => "poke_buttons" }
     end
   end
 end
