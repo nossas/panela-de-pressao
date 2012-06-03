e505ab2b45a3933f35a0b3bf2be2d6cf455bc75c
Nícolas Iensen <nicolas@engage.is>
2012-06-03 10:48:28 -0300

    Add share buttons to campaigns#show

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,6 +7,7 @@ var App = window.App = {
         $(".campaign_twitter_text_status").html(100 - data.input + " caracteres");
       });
 
+      // FB share
       (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
@@ -15,6 +16,16 @@ var App = window.App = {
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
 
+      // Twitter share
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+      // Google share
+      window.___gcfg = {lang: 'pt-BR'};
+      (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      })();
     },
 
     finish: function(){
