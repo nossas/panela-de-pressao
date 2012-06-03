b47783e235024e86698e92b710f3e4821557421b
Nícolas Iensen <nicolas@engage.is>
2012-06-03 10:59:12 -0300

    Add GA

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -26,6 +26,17 @@ var App = window.App = {
         po.src = 'https://apis.google.com/js/plusone.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
       })();
+
+      // Google analytics
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-26278513-4']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
     },
 
     finish: function(){
