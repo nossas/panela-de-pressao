c373a9533630d779a62570ec097e2080c7db931b
Nícolas Iensen <nicolas@engage.is>
2012-06-06 19:55:11 -0300

    Change Facebook permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -19,6 +19,6 @@ end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :meurio, ENV["MEURIO_ID"], ENV["MEURIO_SECRET"]
-  provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream"
+  provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream,user_about_me,user_birthday,user_education_history,user_hometown,user_interests,user_location,user_relationships,user_relationship_details,user_religion_politics,user_work_history"
   provider :twitter, ENV["TWITTER_ID"], ENV["TWITTER_SECRET"]
 end
