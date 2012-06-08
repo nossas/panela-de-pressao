07d76338d02601a45b8cd0719808bbe794f93108
Nícolas Iensen <nicolas@engage.is>
2012-06-08 20:12:41 -0300

    Mail config

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ManifesteSe::Application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
