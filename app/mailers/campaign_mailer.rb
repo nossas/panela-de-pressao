5be1891aff5d6d62e3b835688db177e13bb4bd64
Nícolas Iensen <nicolas@engage.is>
2012-06-10 13:19:40 -0300

    Emails, emails and more emails...

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class CampaignMailer < ActionMailer::Base
   default from: "from@example.com"
 
@@ -9,4 +10,23 @@ class CampaignMailer < ActionMailer::Base
       :from => "contato@seurio.com.br"
     )
   end
+
+  def campaign_awaiting_moderation campaign
+    @campaign = campaign
+    mail(
+      :to => "curadoria@seurio.com.br",
+      :subject => "Campanha aguardando moderação",
+      :from => "contato@seurio.com.br"
+    )
+  end
+
+  def we_received_your_campaign campaign
+    @campaign = campaign
+    mail(
+      :to => campaign.user.email,
+      :subject => "Recebemos a sua campanha",
+      :from => "contato@seurio.com.br"
+    )
+  end
+
 end
