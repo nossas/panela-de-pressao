402e9564dc88e67058cfa264e0b7b88f28619b03
Nícolas Iensen <nicolas@engage.is>
2012-07-27 13:14:58 -0300

    Export users by campaign

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   inherit_resources
   load_and_authorize_resource
   before_filter only: [:update] { return render nothing: true, status: :unauthorized unless current_user and current_user == @user }
+  has_scope :by_campaign_id
 
   def index
     authorize! :export, @current_user if params[:format] == "csv"
