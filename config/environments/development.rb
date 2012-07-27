ab615bd8bd40baff9d4b9859b05a0d5d7a7e59cf
Nícolas Iensen <nicolas@engage.is>
2012-07-27 10:43:04 -0300

    Change default host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,5 @@ ManifesteSe::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  default_url_options[:host] = "paneladepressao.com.br"
+  default_url_options[:host] = "paneladepressao.org.br"
 end
