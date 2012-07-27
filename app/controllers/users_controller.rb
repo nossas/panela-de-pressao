b84001cad1e258286bdca2ffae74fd55b599aedf
Nícolas Iensen <nicolas@engage.is>
2012-07-27 12:25:44 -0300

    Export all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   inherit_resources
   load_and_authorize_resource
   before_filter only: [:update] { return render nothing: true, status: :unauthorized unless current_user and current_user == @user }
+
+  def index
+    authorize! :export, @current_user if params[:format] == "csv"
+    respond_to do |format|
+      format.csv { render :layout => false }
+    end
+  end
 end
