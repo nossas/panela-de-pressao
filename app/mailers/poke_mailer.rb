2b3acc6e32336156d096b760b949abb63df902ba
Luiz Fonseca <runeroniek@gmail.com>
2012-07-31 14:59:53 -0300

    Don't map influencers that doesn't have email.

--- app/mailers/poke_mailer.rb
+++ app/mailers/poke_mailer.rb
@@ -6,7 +6,7 @@ class PokeMailer < ActionMailer::Base
   def poke(the_poke)
     @poke = the_poke
     mail(
-      :to => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>"}.join(", "), 
+      :to => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>" if t.influencer.email != "" }.join(", "), 
       :subject => the_poke.campaign.name,
       :from => "\"#{the_poke.user.name}\" <#{the_poke.user.email}>"
     )
