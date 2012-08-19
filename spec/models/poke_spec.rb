47ed664910075cc603933f72d4cf3a5bb24feb43
Luiz Fonseca <runeroniek@gmail.com>
2012-08-19 17:12:30 -0300

    Add time window restriction for pokes
    An user is not allowed to poke multiple times.
    The user should wait 15 minutes to poke again.

--- spec/models/poke_spec.rb
+++ spec/models/poke_spec.rb
@@ -24,4 +24,23 @@ describe Poke do
       it{ should be_true }
     end
   end
+
+
+  describe "#poked_recently" do
+    context "When the user hasn't poked yet" do
+      it "No error should be thrown" do
+        expect(subject).to have(0).error_on(:created_at)
+      end
+    end
+    context "When the user has poked the same channel in less than 15 minutes" do
+      let(:poke) { Poke.make!(created_at: Time.now - 10.minutes) }
+      subject { Poke.create(created_at: Time.now, user_id: poke.user.id, campaign_id: poke.campaign.id, kind: "facebook") }
+
+      it "Should throw an error saying 'you poked this channel recently'" do
+        expect(subject).to have(1).error_on(:created_at)
+        expect(subject.errors[:created_at]).to eq([I18n.t("activerecord.errors.models.poke.attributes.created_at.poked_recently")])
+      end
+    end
+  end
+
 end
