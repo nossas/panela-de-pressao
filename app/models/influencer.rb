296078b4691289e09fb5f506c7232a9b44a4057e
Nicolas Iensen <nicolas@engage.is>
2012-10-29 11:22:11 -0200

    Handle exception in facebook influencers validation

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -40,7 +40,6 @@ class Influencer < ActiveRecord::Base
   def update_facebook url, options = {}
     graph = Koala::Facebook::API.new ENV["FB_TOKEN"]
     page = graph.get_object(url.match(/(?:http:\/\/)?(?:www\.)?facebook\.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-]*\/)*([\w\-]*)/)[1])
-		end
     if page["can_post"]
 			self.update_column :facebook_url, page["link"]
       self.update_column :facebook_id, page["id"]
