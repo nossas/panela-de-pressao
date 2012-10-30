1c4bd693fda3140664350ac209d06fb496904f60
Nicolas Iensen <nicolas@engage.is>
2012-10-30 16:02:50 -0200

    Add html formatting to campaigns answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,11 @@
 class Answer < ActiveRecord::Base
+  include AutoHtml
   attr_accessible :campaign_id, :text
+  before_save do 
+		self.text_html = auto_html(text) do
+      image
+      youtube(width: "100%", height: 300)
+      redcarpet :target => :_blank
+		end
+	end
 end
