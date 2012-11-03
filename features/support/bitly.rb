8ca2d47857580a50f523fa1bc3fa71542f2a6130
Nícolas Iensen <nicolas@engage.is>
2012-11-03 16:03:52 -0200

    Fix travis

--- features/support/bitly.rb
+++
@@ -1,6 +0,0 @@
-Before('@bitly') do
-  bitly = Bitly.new('bitly id', 'bitly key')
-  bitly.stub_chain(:shorten, :short_url).and_return("http://localhost:3000/campaigns")
-  Bitly.stub(:new).and_return(bitly)
-end
-
