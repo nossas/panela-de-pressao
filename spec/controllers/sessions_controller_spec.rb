1c75eb53bb54505ee1b9a1964e7856c07d4857a0
Nícolas Iensen <nicolas@engage.is>
2012-11-03 16:24:24 -0200

    Fix travis

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe SessionsController do
   subject{ response }
 
+  before do
+    bitly = Bitly.new('bitly id', 'bitly key')
+    bitly.stub_chain(:shorten, :short_url).and_return("http://localhost:3000/campaigns")
+    Bitly.stub(:new).and_return(bitly)
+  end
+
   describe "GET /auth/facebook/callback" do
     let(:poke) { { :user_id => user.id.to_s, :kind => 'email', :campaign_id => Campaign.make!.id } }
     let(:user) { User.make! }
