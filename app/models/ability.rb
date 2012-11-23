153dc16513bce1e097d69bdecb9691b21e87616f
Luiz Fonseca <runeroniek@gmail.com>
2012-11-23 18:53:39 -0200

    Ad preview code to campaigns. Ability to the user to preview his campaign.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,16 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize(user, options = nil)
     can :read, User
     can :read, Influencer
     can :read, Campaign, Campaign.accepted do |campaign|
       campaign.accepted_at
     end
+    can :read, Campaign, Campaign.unmoderated do |campaign|
+      !campaign.preview_code.nil? and campaign.preview_code == options[:preview_code]    
+    end
+
     can :read, Influencer
     can :create, Poke, :kind => "email"
 
