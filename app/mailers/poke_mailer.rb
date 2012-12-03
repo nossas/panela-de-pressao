d952291622b6121987ff0d35b953890f2964106e
Nícolas Iensen <nicolas@engage.is>
2012-12-03 16:44:51 -0200

    Email poke solved

--- app/mailers/poke_mailer.rb
+++ app/mailers/poke_mailer.rb
@@ -7,8 +7,7 @@ class PokeMailer < ActionMailer::Base
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"poke\"] }"
     @poke = the_poke
     mail(
-      :to => "nicolas@engage.is",
-      :cc => the_poke.campaign.targets.map{|t| "'#{t.influencer.name}' <#{t.influencer.email}>" if !t.influencer.email.blank? }.join(", "), 
+      :to => the_poke.campaign.targets.map{|t| "'#{t.influencer.name}' <#{t.influencer.email}>" if !t.influencer.email.blank? }.join(", "), 
       :subject => the_poke.campaign.name,
       :from => "\"#{the_poke.user.name}\" <#{the_poke.user.email}>"
     )
