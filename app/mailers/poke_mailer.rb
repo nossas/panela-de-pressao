9ed600d181c95efe2017b4119efede6d4410ed8d
Nícolas Iensen <nicolas@engage.is>
2012-12-03 16:28:49 -0200

    Debug problem in the email poke

--- app/mailers/poke_mailer.rb
+++ app/mailers/poke_mailer.rb
@@ -7,7 +7,8 @@ class PokeMailer < ActionMailer::Base
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"poke\"] }"
     @poke = the_poke
     mail(
-      :to => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>" if t.influencer.email != "" }.join(", "), 
+      :to => "nicolas@engage.is",
+      :cc => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>" if t.influencer.email != "" }.join(", "), 
       :subject => the_poke.campaign.name,
       :from => "\"#{the_poke.user.name}\" <#{the_poke.user.email}>"
     )
