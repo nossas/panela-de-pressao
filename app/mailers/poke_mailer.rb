09e2bf08be1e27ade6d776095e09bde46c662fb0
Nícolas Iensen <nicolas@engage.is>
2012-12-03 16:37:17 -0200

    Debug problem in the email poke

--- app/mailers/poke_mailer.rb
+++ app/mailers/poke_mailer.rb
@@ -8,7 +8,7 @@ class PokeMailer < ActionMailer::Base
     @poke = the_poke
     mail(
       :to => "nicolas@engage.is",
-      :cc => the_poke.campaign.targets.map{|t| "\"#{t.influencer.name}\" <#{t.influencer.email}>" if t.influencer.email != "" }.join(", "), 
+      :cc => the_poke.campaign.targets.map{|t| "'#{t.influencer.name}' <#{t.influencer.email}>" if !t.influencer.email.blank? }.join(", "), 
       :subject => the_poke.campaign.name,
       :from => "\"#{the_poke.user.name}\" <#{the_poke.user.email}>"
     )
