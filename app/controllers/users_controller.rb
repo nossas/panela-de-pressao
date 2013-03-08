6e9edd1926ac20dc6ff6cb27f12424c7b4965314
Nícolas Iensen <nicolas@engage.is>
2013-03-08 13:58:38 -0300

    Admin can edit any user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   inherit_resources
   load_and_authorize_resource
-  before_filter only: [:update] { return render nothing: true, status: :unauthorized unless current_user and current_user == @user }
+  before_filter only: [:update] { return render nothing: true, status: :unauthorized unless can?(:update, @user) }
   has_scope :by_campaign_id
 
   def index
