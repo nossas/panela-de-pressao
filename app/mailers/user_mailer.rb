4189e93634f183e42ce0c037e4a8f3c942f8b8fd
Nícolas Iensen <nicolas@engage.is>
2013-03-19 15:33:21 -0300

    Add sender name in the recomendations email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,13 @@
 # coding: utf-8
 
 class UserMailer < ActionMailer::Base
+  default from: "Panela de Pressão <contato@paneladepressao.org.br>"
   layout 'mailer'
 
   def recomendations(user_id)
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"recomendations\"] }"
     @user = User.find user_id
     @recomendations = Recomendation.where(:user_id => @user.id).order("RANDOM()").limit(3)
-
-    mail(
-      :to => @user.email,
-      :subject => "Veja as campanhas que separamos essa semana para você",
-      :from => "contato@paneladepressao.org.br"
-    )
+    mail(:to => @user.email, :subject => "Veja as campanhas que separamos essa semana para você")
   end
 end
