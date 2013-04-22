9da8cc991598082bc739301fd2213298b3a7e6e0
Nícolas Iensen <nicolas@engage.is>
2013-04-22 11:15:54 -0300

    Bug fix when creating a new authorization for an existing user

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -9,7 +9,7 @@ class Authorization < ActiveRecord::Base
   end
 
   def self.create_from_hash(hash, user = nil)
-    user ||= User.create_from_hash!(hash)
+    user ||= User.find_by_email(hash['info']['email']) || User.create_from_hash!(hash)
     Authorization.create(:user => user, :uid => hash['uid'], :provider => hash['provider'], :token => hash['credentials']['token'], :secret => hash['credentials']['secret'])
   end
 end
