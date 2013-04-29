94f436b21f5ba7f3c2561c2cb39dbfcd60021e7f
Nícolas Iensen <nicolas@engage.is>
2013-04-29 14:46:37 -0300

    Change permissions to change influencer's profile

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,13 +1,4 @@
 class InfluencersController < InheritedResources::Base
-  load_and_authorize_resource
-
-	def update
-		@influencer.user_id = current_user.id
-		update!
-	end
-
-	def create
-		@influencer.user_id = current_user.id
-		create!
-	end
+  authorize_resource
+  before_filter(only: [:create, :update]) { params[:influencer][:user_id] = current_user.id }
 end
