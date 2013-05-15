eae18fc8fb1a2cd093a083d98246e61f903f31df
Nícolas Iensen <nicolas@engage.is>
2013-05-15 18:44:54 -0300

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream,user_about_me,user_birthday,user_education_history,user_hometown,user_interests,user_location,user_relationships,user_relationship_details,user_religion_politics,user_work_history"
+  provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream,user_birthday,user_education_history,user_interests,user_location,user_religion_politics"
   provider :twitter, ENV["TWITTER_ID"], ENV["TWITTER_SECRET"]
 end
