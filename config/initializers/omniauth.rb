bb303ffd2dfa25b6ab49df672f332ee4b77ba236
Nícolas Iensen <nicolas@iensen.me>
2013-09-03 17:41:32 -0300

    [#56218466] remove user_birthday, user_education_history, user_interests, user_location, user_religion_politics from Facebook connection

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ module OmniAuth::Strategies
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream,user_birthday,user_education_history,user_interests,user_location,user_religion_politics"
+  provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream"
   provider :twitter, ENV["TWITTER_ID"], ENV["TWITTER_SECRET"]
-  provider :facebook_admin, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream,user_birthday,user_education_history,user_interests,user_location,user_religion_politics,manage_pages"
+  provider :facebook_admin, ENV["FB_ID"], ENV["FB_SECRET"], :scope => "email,publish_stream,manage_pages"
 end
