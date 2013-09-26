a386f0ffb7a2f7a884ad74b3ef3bda6d2083823a
Nícolas Iensen <nicolas@iensen.me>
2013-09-26 19:21:03 -0300

    [#57766944] remove targets links

--- spec/controllers/influencers_controller_spec.rb
+++ spec/controllers/influencers_controller_spec.rb
@@ -15,11 +15,11 @@ describe InfluencersController do
 
       context "and the user is logged in" do
         let(:user) { User.new }
-        
+
         before do
           User.stub(:find_by_id).and_return(user)
         end
-        
+
         context "and can edit influencers" do
           before do
             user.admin = true
@@ -28,33 +28,7 @@ describe InfluencersController do
 
           it { should respond_with(200) }
         end
-
-        context "and cannot edit influencers" do
-          before do
-            get :show, id: 1
-          end
-          
-          it { should redirect_to influencers_path }
-        end
       end
-
-      context "and the user is not logged in" do
-        before do
-          get :show, id: 1
-        end
-        
-        it { should redirect_to influencers_path }
-      end
-    end
-
-    context "when the influencer is not archived" do
-      before do
-        influencer.stub(:archived?).and_return(false)
-
-        get :show, id: 1
-      end
-
-      it { should respond_with(200) }
     end
   end
 end
