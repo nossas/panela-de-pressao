7f98e692736d81793d56f24b4b0f0763c593af45
Nícolas Iensen <nicolas@iensen.me>
2013-11-08 08:32:03 -0200

    Fix migration

--- db/migrate/20131104210424_alter_table_campaigns_drop_constraint_fk_campaigns_moderator_id.rb
+++ db/migrate/20131104210424_alter_table_campaigns_drop_constraint_fk_campaigns_moderator_id.rb
@@ -1,7 +1,7 @@
 class AlterTableCampaignsDropConstraintFkCampaignsModeratorId < ActiveRecord::Migration
   def up
     execute <<-SQL
-        ALTER TABLE campaigns DROP CONSTRAINT fk_campaigns_moderator_id;
+        ALTER TABLE campaigns DROP CONSTRAINT IF EXISTS fk_campaigns_moderator_id;
     SQL
   end
 
