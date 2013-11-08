d157df3bbfbf4c1ab1ad8d3f4718a5b03254279a
Nícolas Iensen <nicolas@iensen.me>
2013-11-08 08:32:03 -0200

    Fix migration

--- db/migrate/20131104201334_alter_table_pokes_drop_constraing_fk_pokes_user_id.rb
+++ db/migrate/20131104201334_alter_table_pokes_drop_constraing_fk_pokes_user_id.rb
@@ -1,7 +1,7 @@
 class AlterTablePokesDropConstraingFkPokesUserId < ActiveRecord::Migration
   def up
     execute <<-SQL
-        ALTER TABLE pokes DROP CONSTRAINT fk_pokes_user_id;
+        ALTER TABLE pokes DROP CONSTRAINT IF EXISTS fk_pokes_user_id;
     SQL
   end
 
