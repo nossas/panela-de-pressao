8c2fb29b5dc74f0c69fb1e54ee1e5e9c28495678
Nícolas Iensen <nicolas@engage.is>
2013-11-13 07:51:21 -0200

    Change secret token and session store

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ManifesteSe::Application.config.secret_token = '0f0afa95664271f59aacc44364caa755d15a8dc0c154542e6dc0d21d00862e36e5d189274ca2ca88f796bfac8089de157906d5aec8e9ff72fed400b4f447a601'
+ManifesteSe::Application.config.secret_token = 'c9a9f838489ee41537e443c03bd7a391e3af30ddc4280452574c5de3c33e87f174473accce3b1c7dc5edf70c8b90c602ffe47b9fe626e57533dc682d469b0855'
