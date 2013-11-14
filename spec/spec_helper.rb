f780b0c3875d2ec8e3545f3b56b459c62c6f627a
Nícolas Iensen <nicolas@engage.is>
2013-11-14 17:48:50 -0200

    All tests working, now let's start the real work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -37,8 +37,5 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.before { ActionMailer::Base.deliveries.clear }
   config.before { Bitly.stub_chain(:new, :shorten, :short_url).and_return("bitly.com/unlockmedia") }
-    
-  config.before do
-    load Rails.root + 'db/seeds.rb'
-  end  
+  config.before { load Rails.root + 'db/seeds.rb' }
 end
