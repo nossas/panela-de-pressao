10f52e430e844777c7fb7b2c1337a2e188fcfff7
Nícolas Iensen <nicolas@engage.is>
2013-11-14 16:46:04 -0200

    Rollback user login and starting from beginning

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+if Rails.env.production?
+  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+else
+  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
