78ba16d7bda2b4ca22e5be550235c7bab040d8d6
Nícolas Iensen <nicolas@iensen.me>
2013-12-10 07:41:52 -0200

    [#61374462] debuging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 1.minute
 else
   ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
 end
