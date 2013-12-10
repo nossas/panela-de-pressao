84a12b5416e185a2df93293ef4ef304bc341027e
Nícolas Iensen <nicolas@iensen.me>
2013-12-10 00:24:28 -0200

    [#62143876] fix tests

--- spec/support/mailchimp.rb
+++ spec/support/mailchimp.rb
@@ -1,5 +1,7 @@
 RSpec.configure do |config|
   config.before do
-    Gibbon::API.stub_chain(:lists, :segment_add).and_return({"id" => 1})  
+    Gibbon::API.stub_chain(:lists, :segment_add).and_return({"id" => 1})
+    Gibbon::API.stub_chain(:lists, :subscribe)
+    Gibbon::API.stub_chain(:lists, :static_segment_members_add)
   end
 end
