ebbe981c280c54d082e05b4505d724ac7cda0fad
Nícolas Iensen <nicolas@iensen.me>
2013-12-10 08:02:10 -0200

    [#61374462] change session expiration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 1.minute
+  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
 else
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
 end
 
 # Use the database for sessions instead of the cookie-based default,
