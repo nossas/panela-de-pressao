7d2196bd663bab5ea1295975ff2de8fcf9f13600
Nícolas Iensen <nicolas@iensen.me>
2013-12-13 10:55:04 -0200

    [#60523972] refactoring to_button helper

--- features/support/mailchimp.rb
+++ features/support/mailchimp.rb
@@ -2,4 +2,5 @@ Before do
   Gibbon::API.stub_chain(:lists, :segment_add).and_return({"id" => 1})
   Gibbon::API.stub_chain(:lists, :subscribe)
   Gibbon::API.stub_chain(:lists, :static_segment_members_add)
+  Gibbon::API.stub_chain(:lists, :segments).and_return(Hash.new("static" => []))
 end
