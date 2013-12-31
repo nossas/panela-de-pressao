4141a2ae0d7ee77522b9acf1464be93c8cbbd8ac
Nícolas Iensen <nicolas@iensen.me>
2013-12-31 16:02:18 -0200

    Turn schema plus foreign key auto creation off when creating reports table

--- db/migrate/20131231145447_create_reports.rb
+++ db/migrate/20131231145447_create_reports.rb
@@ -1,5 +1,8 @@
 class CreateReports < ActiveRecord::Migration
   def change
+    SchemaPlus.config.foreign_keys.auto_create = false
+    SchemaPlus.config.foreign_keys.auto_index = false
+
     create_table :reports do |t|
       t.integer :campaign_id
       t.integer :user_id
