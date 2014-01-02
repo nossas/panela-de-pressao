546dbbf75c72c7d0f6cffaf2b53e00d5b0e9851f
Nícolas Iensen <nicolas@iensen.me>
2014-01-02 12:42:18 -0200

    Add feature 'report a campaign'; add controller Reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,8 @@
 class Report < ActiveRecord::Base
   attr_accessible :campaign_id, :user_id
 
+  validates :campaign_id, :user_id, presence: true
+
   belongs_to :campaign
   belongs_to :user
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Report do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :campaign_id }
 end
