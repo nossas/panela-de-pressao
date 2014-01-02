333faa331844b05310c25b06c198386797937254
Josias Schneider <josias.ds@gmail.com>
2014-01-02 16:49:28 -0200

    [#62544066] change 'we received your campaign' email

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -7,30 +7,30 @@ class CampaignMailer < ActionMailer::Base
   def campaign_accepted campaign
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"campaign_accepted\"] }"
     @campaign = campaign
-    mail(:to => campaign.user.email, :subject => "Sua campanha foi aprovada!")
+    mail(to: campaign.user.email, subject: "Sua campanha foi aprovada!")
   end
 
   def campaign_awaiting_moderation campaign
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"campaign_awaiting_moderation\"] }"
     @campaign = campaign
-    mail(:to => "curadoria@paneladepressao.org.br", :subject => "Campanha aguardando moderação")
+    mail(to: "curadoria@paneladepressao.org.br", subject: "Campanha aguardando moderação")
   end
   
   def campaigns_without_moderator
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"campaigns_without_moderator\"] }"
     @campaigns = Campaign.orphan.unmoderated.unarchived
-    mail(:to => "curadoria@paneladepressao.org.br", :subject => "Campanhas sem moderador")
+    mail(to: "curadoria@paneladepressao.org.br", subject: "Campanhas sem moderador")
   end
 
   def we_received_your_campaign campaign
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"we_received_your_campaign\"] }"
     @campaign = campaign
-    mail(:to => campaign.user.email, :subject => "Recebemos a sua campanha")
+    mail(to: campaign.user.email, subject: "A sua mobilização está no ar!", from: "Fernanda do Meu Rio <contato@meurio.org.br>")
   end
 
   def report campaign
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"campaign_report\"] }"
     @campaign = campaign
-    mail(:to => campaign.user.email, :subject => "Relatório da sua campanha")
+    mail(to: campaign.user.email, subject: "Relatório da sua campanha")
   end
 end
