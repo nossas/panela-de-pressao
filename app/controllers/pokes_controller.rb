eb46908e1f6113e8be52d722a0f9a6c3a5ce4647
Josias Schneider <josias.ds@gmail.com>
2014-03-21 15:14:43 -0300

    fix Poke user password generator

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -25,7 +25,7 @@ class PokesController < InheritedResources::Base
 
       unless user
         url = "#{ENV["ACCOUNTS_HOST"]}/users.json"
-        user_hash = { first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: Devise.friendly_token }
+        user_hash = { first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: SecureRandom.hex }
         body = { token: ENV["ACCOUNTS_API_TOKEN"], user: user_hash }
         response = HTTParty.post(url, body: body, headers: { 'Content-Type' => 'application/json' })
         user = User.find_by_id(response['id'])
