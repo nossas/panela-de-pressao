b676ec6faf20f9203c9545ccf5789e7210c982e3
Josias Schneider <josias.ds@gmail.com>
2014-05-14 10:41:18 -0300

    [#70792034] fix mocks and tests

--- features/support/mailchimp.rb
+++ features/support/mailchimp.rb
@@ -1,5 +1,6 @@
 Before do
   Gibbon::API.stub_chain(:lists, :segment_add).and_return({"id" => 1})
+  Gibbon::API.stub_chain(:lists, :segment_update).and_return({"complete" => true})
   Gibbon::API.stub_chain(:lists, :subscribe)
   Gibbon::API.stub_chain(:lists, :static_segment_members_add)
   Gibbon::API.stub_chain(:lists, :segments).and_return(Hash.new("static" => []))
