6e30f6861d29057d6223a59eb14e2d4d2854e96b
Josias Schneider <josias.ds@gmail.com>
2014-05-22 13:23:30 -0300

    [#71774894] update gem Carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,16 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV["AWS_ID"],
-    :aws_secret_access_key  => ENV["AWS_SECRET"],
-  }
-  config.fog_directory  = ENV["AWS_BUCKET"] 
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-
-  if Rails.env.test? or Rails.env.cucumber?
-    CarrierWave.configure do |config|
-      config.storage = :file
-      config.enable_processing = false
-    end
+  if Rails.env.development? or Rails.env.test? or Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+  else
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV["AWS_ID"],
+      :aws_secret_access_key  => ENV["AWS_SECRET"],
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
 end
