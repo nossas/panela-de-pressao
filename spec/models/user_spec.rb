1b2cfd4c7d0df288c39972b50c6f0a6ac786133c
Nícolas Iensen <nicolas@iensen.me>
2014-05-23 17:01:10 -0300

    [#71172128] remove User#can_poke?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,21 +4,6 @@ describe User do
   it { should have_many :authorizations }
   it { should have_many :campaigns }
 
-  describe "#can_poke?" do
-    subject { User.make! }
-    let(:campaign) { Campaign.make! }
-
-    context "when there is no poke in the last 24h" do
-      it("should be true") { subject.can_poke?(campaign).should be_true }
-    end
-
-    context "when there is at least one poke in the last 24h" do
-      before { Target.make! campaign: campaign }
-      before { Poke.make! user: subject, campaign: campaign }
-      it("should be false") { subject.can_poke?(campaign).should be_false }
-    end
-  end
-
   describe ".by_campaign_id" do
     let(:user)      { User.make! }
     let(:campaign)  { Campaign.make! }
