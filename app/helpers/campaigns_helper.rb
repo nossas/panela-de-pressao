008ea95e7afb65f31e90eb32da250cca37f725e2
Nícolas Iensen <nicolas@iensen.me>
2014-06-09 15:06:23 -0300

    [#72687088] add user links

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -2,4 +2,8 @@ module CampaignsHelper
   def moderated_class? campaign
   	'moderated' if campaign.moderated?
   end
+
+  def user_path user
+    "#{ENV['MEURIO_ACCOUNTS_URL']}/users/#{user.id}"
+  end
 end
