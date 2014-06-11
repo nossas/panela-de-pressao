125de5521399903c5cc3ff05a1233ca47145f27b
Nícolas Iensen <nicolas@iensen.me>
2014-06-11 15:11:51 -0300

    [#72785616] change mailer copy

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -6,6 +6,8 @@ class ReportMailer < ActionMailer::Base
   def new_report report
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"new_report\"] }"
     @report = report
-    mail(:to => "curadoria@paneladepressao.org.br", :subject => "Nova denúncia")
+    @organization = report.campaign.organization
+    to = @organization.pdp_receiver_email.present? ? @organization.pdp_receiver_email : "curadoria@paneladepressao.org.br"
+    mail(:to => to, :subject => "Nova denúncia no Panela (#{@organization.city})")
   end
 end
