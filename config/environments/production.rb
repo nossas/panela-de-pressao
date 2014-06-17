4cec4fb22bd24e58bf8657b8bdf4482ff4ca2644
Nícolas Iensen <nicolas@iensen.me>
2014-06-17 10:59:59 -0300

    [#71260842] change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ ManifesteSe::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => "paneladepressao.meurio.org.br",
+    :domain => "paneladepressao.minhascidades.org.br",
     :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
+  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'minhascidades.org.br', expire_after: 100.years
 elsif Rails.env.staging?
   ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
 else
