bfcae9035aeaa341171c8f010b8030dc68c36030
Nícolas Iensen <nicolas@iensen.me>
2014-06-18 16:40:11 -0300

    Add rack-cas and implement CAS login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.production?
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'minhascidades.org.br', expire_after: 100.years
-elsif Rails.env.staging?
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
-else
-  ManifesteSe::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
-end
+ManifesteSe::Application.config.session_store :cookie_store, key: '_manifeste.se_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
