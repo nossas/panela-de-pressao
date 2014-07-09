383814af2dd78aa7f4c104ace36e1d2d8be2cfc0
Nícolas Iensen <nicolas@iensen.me>
2014-07-09 09:21:46 -0300

    [#74520224] add mailchimp list id to organizations and add support for Mailchimp lists for poke integration

--- app/models/poke.rb
+++ app/models/poke.rb
@@ -83,8 +83,22 @@ class Poke < ActiveRecord::Base
 
   def add_to_mailchimp_segment
     begin
-      Gibbon::API.lists.subscribe(id: ENV["MAILCHIMP_LIST_ID"], email: {email: self.user.email}, merge_vars: {FNAME: self.user.first_name, LNAME: self.user.last_name}, double_optin: false, update_existing: true)
-      Gibbon::API.lists.static_segment_members_add(id: ENV["MAILCHIMP_LIST_ID"], seg_id: self.campaign.mailchimp_segment_uid, batch: [{email: self.user.email}])
+      Gibbon::API.lists.subscribe(
+        id: self.campaign.organization.mailchimp_list_id,
+        email: { email: self.user.email },
+        merge_vars: {
+          FNAME: self.user.first_name,
+          LNAME: self.user.last_name
+        },
+        double_optin: false,
+        update_existing: true
+      )
+
+      Gibbon::API.lists.static_segment_members_add(
+        id: self.campaign.organization.mailchimp_list_id,
+        seg_id: self.campaign.mailchimp_segment_uid,
+        batch: [{ email: self.user.email }]
+      )
     rescue Exception => e
       Rails.logger.error e
     end
---
+++ db/migrate/20140709120243_add_mailchimp_list_id_to_organizations.rb
@@ -0,0 +1,5 @@
+class AddMailchimpListIdToOrganizations < ActiveRecord::Migration
+  def change
+    add_column :organizations, :mailchimp_list_id, :string, foreign_key: false
+  end
+end
