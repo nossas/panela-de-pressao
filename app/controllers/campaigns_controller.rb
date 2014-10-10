03388a872913623a4ab985fdf8d46e998c9f6cea
Nícolas Iensen <nicolas@iensen.me>
2014-10-10 11:58:01 -0300

    [#80382014] fix pagination with form filters

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -79,7 +79,10 @@ class CampaignsController < InheritedResources::Base
 
   def explore
     @campaigns = Campaign.unarchived
+
     @campaigns = @campaigns.successful if params[:successful]
+    @campaigns = @campaigns.unfinished if params[:unfinished]
+
     @campaigns = @campaigns.where(organization_id: params[:organizations]) if params[:organizations].present?
     @campaigns = @campaigns.where(category_id: params[:categories]) if params[:categories].present?
 
