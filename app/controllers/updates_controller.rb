94ff9feefaa7c637fded07f99ffc151e61c8aa1b
Josias Schneider <josias.ds@gmail.com>
2014-10-24 17:39:22 -0200

    [#79922744] allow campaign's owner to create campaign updates

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,10 +1,11 @@
 # coding: utf-8
 class UpdatesController < InheritedResources::Base
+  load_and_authorize_resource
   layout false
   belongs_to :campaign
-  before_filter(only: ["create"]) { params[:update][:user_id] = current_user.id }
 
   def create
+    @update = Update.new(params[:update].merge(user_id: current_user.id, campaign_id: parent.id))    
     create! do |format|
       format.html { redirect_to updates_campaign_path(@campaign, anchor: "update_#{@update.id}") }
     end
