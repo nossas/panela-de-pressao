24525e45fcfcd41f474092d1c640947dade316d8
Nícolas Iensen <nicolas@iensen.me>
2014-10-28 13:54:04 -0200

    [#72720532] change influencer#show page layout

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -3,6 +3,11 @@ class InfluencersController < InheritedResources::Base
   before_filter(only: [:create, :update]) { params[:influencer][:user_id] = current_user.id }
   before_filter :redirect_when_archived, only: :show
 
+  def show
+    @influencer = Influencer.find(params[:id])
+    @campaigns = @influencer.campaigns.unarchived
+  end
+
   private
     def redirect_when_archived
       redirect_to influencers_path if resource.archived? && !can?(:edit, Influencer)
