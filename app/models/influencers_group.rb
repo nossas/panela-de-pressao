77a7be678f8fd362535531ce4f08897173e16443
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-11-11 11:17:00 -0200

    [#81848514] add influencers groups support for influencers autocomplete

--- app/models/influencers_group.rb
+++ app/models/influencers_group.rb
@@ -1,4 +1,9 @@
 class InfluencersGroup < ActiveRecord::Base
+  attr_accessible :name
+
   validates :name, presence: true, uniqueness: true
-  has_and_belongs_to_many :influencers
+  has_and_belongs_to_many :influencers, -> { uniq }
+
+  include PgSearch
+  multisearchable against: [:name]
 end
