322f45bc13e962544d2cea5fba958417ed2911b1
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-03-23 17:28:26 -0300

    Fix event tracking javascript

--- app/assets/javascripts/event_tracking.js
+++ app/assets/javascripts/event_tracking.js
@@ -1,9 +1,9 @@
 $(function(){
   $("#GooglePlayButton").click(function(){
-    _gaq.push(['_trackEvent', 'Fake Buttons', 'Download Android App']);
+    ga('send', 'event', 'Fake Buttons', 'Download Android App');
   });
 
   $("#AppStoreButton").click(function(){
-    _gaq.push(['_trackEvent', 'Fake Buttons', 'Download iPhone App']);
+    ga('send', 'event', 'Fake Buttons', 'Download iPhone App');
   });
 });
