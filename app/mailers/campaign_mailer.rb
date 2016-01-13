df46e8ec272933a6d5035f53898498aa53924c43
Leticia Figueira <jlcfigueira@gmail.com>
2016-01-13 17:09:27 -0200

    Fixes autofire emails whenever sender and receiver are not configured.

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -8,7 +8,7 @@ class CampaignMailer < ActionMailer::Base
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"we_received_your_campaign\"] }"
     @campaign = campaign
     @organization = @campaign.organization
-    from = @organization.pdp_sender_email.present? ? @organization.pdp_sender_email : "João Mauro do Meu Rio <contato@meurio.org.br>"
+    from = @organization.try(:pdp_sender_email) ? @organization.pdp_sender_email : "João Mauro do Meu Rio <contato@meurio.org.br>"
     mail(to: campaign.user.email, subject: "A sua mobilização está no ar!", from: from)
   end
 
@@ -16,7 +16,7 @@ class CampaignMailer < ActionMailer::Base
     headers "X-SMTPAPI" => "{ \"category\": [\"pdp\", \"new_campaign\"] }"
     @campaign = campaign
     @organization = @campaign.organization
-    to = @organization.pdp_receiver_email.present? ? @organization.pdp_receiver_email : "curadoria@paneladepressao.org.br"
+    to = @organization.try(:pdp_receiver_email) ? @organization.pdp_receiver_email : "curadoria@paneladepressao.org.br"
     mail(to: to, subject: "Nova campanha no Panela de Pressão (#{@organization.city})")
   end
 end
