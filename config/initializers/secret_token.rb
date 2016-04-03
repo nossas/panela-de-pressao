e363fed44d8d18dc6699917efad2feb0f6e35d63
Leticia Figueira <jlcfigueira@gmail.com>
2016-04-03 13:02:18 -0300

    Upgrades the way application secrets are managed

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ManifesteSe::Application.config.secret_token = 'c9a9f838489ee41537e443c03bd7a391e3af30ddc4280452574c5de3c33e87f174473accce3b1c7dc5edf70c8b90c602ffe47b9fe626e57533dc682d469b0855'
